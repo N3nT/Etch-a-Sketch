85dabc5874e46d83419796e835f4b72496d4aba8
N3nT <bar55v71@interia.pl>
2025-02-12 15:40:23 +0100

    continue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
+const mainPad = document.querySelector('.mainPad');
 const cells = document.querySelectorAll(".cell");
+let cellsNumberInput = document.querySelector('.cellsNum');
+const cell = document.createElement("div");
+cell.className = "cell border-2 p-5";
 
-cells.forEach(cell => {
-  cell.addEventListener('mouseover', () => {
-    cell.classList.add('bg-black');
-  })
-});
+const getCellsNumber = () => {
+  let cellsNumber = cellsNumberInput.value;
+  mainPad.classList.add(`grid-cols-${cellsNumber}`);
+  console.log(cellsNumber * cellsNumber);
+  for(let i = 0; i < cellsNumber * cellsNumber - 1; i++){
+    console.log(i);
+    mainPad.appendChild(cell);
+  }
+  cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+      cell.classList.add('bg-black');
+    })
+  });
+}
+
+cellsNumberInput.addEventListener('change', getCellsNumber);
+//cell border-2 p-5 - cell
+//grid-cols-4 - mainPad
